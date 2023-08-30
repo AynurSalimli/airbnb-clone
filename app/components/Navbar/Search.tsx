521f57d4a6303829edab0298b3ebebb73826f176
unknown <aynur.selimli@ascca.edu.az>
2023-08-30 17:45:52 +0400

    nav is processing

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -1,8 +1,9 @@
 'use client';
+import { BiSearch } from 'react-icons/bi'
 const Search = () => {
     return (
         <div
-            className="
+        className="
         border-[1px]
         w-full
         md:w-auto
@@ -13,17 +14,58 @@ const Search = () => {
         transition
         cursor-pointer
         ">
-            <div className="flex flex-row items-center justify-between">
-                <div className="text-sm font-semibold px-6">
+            <div
+                className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            "
+            >
+                <div className="
+                text-sm 
+                font-semibold 
+                px-6">
                     Anywhere
                 </div>
-                <div className="hidden sm:block text-sm font-semibold px-6 border-x-[-1px] flex-1 text-center">
+                <div className="
+                hidden 
+                sm:block 
+                text-sm 
+                font-semibold 
+                px-6 
+                border-x-[-1px] 
+                flex-1 
+                text-center">
                     Any Week
                 </div>
-                <div className="text-sm
-                pl-6 pr-2 text-gray-600 flex-row items-center gap-3">
-                    <div className="hidden sm:block ">Add Guests</div>
+                <div className="
+                text-sm
+                pl-6
+                pr-2 
+                text-gray-600 
+                flex-row 
+                items-center 
+                gap-3
+                "
+                >
+                    <div
+                    className="
+                    hidden 
+                    sm:block
+                    ">
+                        Add Guests
+                    </div>
                 </div>
+                    <div className="
+                    p-2
+                    bg-rose-500
+                    rounded-full
+                    text-white
+                    "
+                    >
+                        <BiSearch size={18} />
+                    </div>
             </div>
         </div>
     );
