7149fd20d9e1abe528d75c398bc9890a7b813c59
unknown <aynur.selimli@ascca.edu.az>
2023-09-01 14:39:10 +0400

    client

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,33 @@
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+
+}
+
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryLabel,
+    secondaryAction
+
+}) => {
+    return (
+        <div></div>
+    )
+}
+
+export default Modal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 
 export const metadata = {
   title: 'Airbnb',
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal/>
+          <Navbar />
+        </ClientOnly>
         {children}</body>
     </html>
   )
