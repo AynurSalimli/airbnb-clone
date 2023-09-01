a69bed1bf5955193aca82be64ef900f2fe04b3a9
unknown <aynur.selimli@ascca.edu.az>
2023-09-01 16:54:44 +0400

    modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal/>
+          <Modal title='Hello World' isOpen/>
           <Navbar />
         </ClientOnly>
         {children}</body>
