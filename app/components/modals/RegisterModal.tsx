dbc9197e2e4faa35c76a944c14111b44093c7d80
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 11:49:42 +0400

    register-modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -85,18 +85,50 @@ const RegisterModal = () => {
         </div>
     )
 
-    const footerContent = () => {
+    const footerContent = (
         <div className="flex flex-col gap-4 mt-3">
             <hr />
             <Button
                 outline
                 label="Continue with google"
                 icon={FcGoogle}
-                onClick={() => { }} />
+                onClick={() => { }}
+            />
+            <Button
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => { }}
+            />
+            <div
+                className="
+            text-neutral-500
+            text-center
+            mt-4
+            font-light">
+                <div className="
+                flex 
+                flex-row
+                items-center
+                gap-2
+                justify-center
+                ">
+                    <div>
+                        Already have an account?
+
+                    </div>
+                    <div className="
+                    text-neutral-800
+                    cursor-pointer
+                    hover:underline"
+                    onClick={registerModal.onClose}>
+                        Log in
+
+                    </div>
+                </div>
+            </div>
         </div>
-    }
-
-
+    )
     return (
         <Modal
             disabled={isLoading}
